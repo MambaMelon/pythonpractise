d120789135a4a9641b3aa1227ab518b605663f4e
MambaMelon <1115431474@qq.com>
2018-05-27 23:27:56 +0800

    numpy创建数组

--- ai/pythonbase/numpy01.py
+++ ai/pythonbase/numpy01.py
@@ -17,13 +17,31 @@ if __name__ == "__main__":
 
     # 创建多维数组
     l4 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    arr1 = np.array(l4)
+    arr01 = np.array(l4)
 
     # zeros函数.创建一个元素全为0.的浮点型数组.int设置它的类型
-    arr2 = np.zeros((3, 2), int)
+    arr02 = np.zeros((3, 2), int)
 
     # ones函数.创建一个元素全为1的数组
-    arr3 = np.ones((3, 2), int)
+    arr03 = np.ones((3, 2), int)
 
-    arr4 = np.empty((2, 2))
-    print(arr4)
+    arr04 = np.empty((2, 2))
+
+    # [0 2 4 6 8].从0开始到10结束,步长为2
+    arr05 = np.arange(0, 10, 2)
+
+    # [ 0.  5. 10.].起始值,最终值以及数组的元素总个数
+    arr06 = np.linspace(0, 10, 3)
+
+    # 3行2列不大于1的浮点数数组
+    arr07 = np.random.rand(3, 2)
+
+    # 生成2行3列数字在[0,5)之间的数组
+    arr08 = np.random.randint(5, size=(2, 3))
+
+    # [0.80570316 0.87428801 0.54497608]
+    arr09 = np.random.random_sample(3)
+
+    # [3 4].从一个给定的数组中生成size大小的数组
+    arr10 = np.random.choice((1, 2, 3, 4, 5), size=2)
+    print(arr10)
