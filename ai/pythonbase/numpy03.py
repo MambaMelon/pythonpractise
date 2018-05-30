07ee96b0280ec0c4408c35c15fc478685ce2dbd1
MambaMelon <1115431474@qq.com>
2018-05-31 06:12:11 +0800

    axis轴的理解

--- ai/pythonbase/numpy03.py
+++ ai/pythonbase/numpy03.py
@@ -25,4 +25,18 @@ if __name__ == "__main__":
     res04 = np.where(condition, 0, arr03)
 
     # 去重函数unique
-    print(res04)
+
+    # 设axis=i，则numpy沿着第i个下标变化的方向进行操作
+    arr04 = np.array([[1, 2, 3], [1, 2, 3]], int)
+    # 12
+    res05 = np.sum(arr04)
+    # [2 4 6].axis=0,00->10,01->11,02->12
+    res06 = np.sum(arr04, axis=0)
+    # [6 6].axis=1,00->01->02,10->11->12
+    res07 = np.sum(arr04, axis=1)
+
+    # 浅复制
+    arr04.view()
+    # 深复制
+    arr04.copy()
+    print(arr04.shape)
