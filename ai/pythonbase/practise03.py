41f7431cb5837b50ac6aea273e3e428c20b7c598
MambaMelon <1115431474@qq.com>
2018-06-04 10:37:24 +0800

    usda_food

--- ai/pythonbase/practise03.py
+++ ai/pythonbase/practise03.py
@@ -5,14 +5,32 @@
 import json
 import numpy as np
 import pandas as pd
+import os
 
 if __name__ == "__main__":
 
-    f = open('F:/datasets/pydata-book-2nd-edition/datasets/usda_food/database.json')
+    f = open(os.getcwd() + '\datasets\\usda_food.json')
     db = json.load(f)
     nutrients = pd.DataFrame(db[0]['nutrients'])
 
     info_keys = ['description', 'group', 'id', 'manufacturer']
     info = pd.DataFrame(db, columns=info_keys)
 
-    print(db[0:2])
+    nutrients = []
+    for rec in db:
+        fnuts = pd.DataFrame(rec['nutrients'])
+        fnuts['id'] = rec['id']
+        nutrients.append(fnuts)
+
+    nutrients = pd.concat(nutrients, ignore_index=True)
+    nutrients.drop_duplicates()
+
+    col_mapping = {'description': 'food', 'group': 'fgroup'}
+    info = info.rename(columns=col_mapping, copy=False)
+
+    col_mapping = {'description': 'nutrient', 'group': 'nutgroup'}
+    nutrients = nutrients.rename(columns=col_mapping, copy=False)
+
+    ndata = pd.merge(nutrients, info, on='id', how='outer')
+
+    print(nutrients)
