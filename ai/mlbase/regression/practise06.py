d0d7101bc14675c8b2169a0b01edb05f9cc19aa2
MambaMelon <1115431474@qq.com>
2018-06-27 09:40:19 +0800

    逻辑回归参数意义

--- ai/mlbase/regression/practise06.py
+++ ai/mlbase/regression/practise06.py
@@ -32,6 +32,17 @@ if __name__ == '__main__':
     ss = StandardScaler()
     ss.fit_transform(x_train)
 
+    # penalty: 过拟合解决参数,l1或者l2
+    # solver: 参数优化方式
+    # 当penalty为l1的时候，参数只能是：liblinear(坐标轴下降法)
+    # nlbfgs和cg都是关于目标函数的二阶泰勒展开
+    # 当penalty为l2的时候，参数可以是：lbfgs(拟牛顿法)、newton-cg(牛顿法变种)，seg(minibatch)
+    # 维度<10000时，lbfgs法比较好，   维度>10000时， cg法比较好，显卡计算的时候，lbfgs和cg都比seg快
+    # multi_class: 分类方式参数；参数可选: ovr(默认)、multinomial；
+    # 这两种方式在二元分类问题中，效果是一样的；在多元分类问题中，效果不一样
+    # ovr: one-vs-rest， 对于多元分类的问题，先将其看做二元分类，分类完成后，再迭代对其中一类继续进行二元分类
+    # multinomial: many-vs-many（MVM）,即Softmax分类效果
+    # class_weight: 特征权重参数
     algo = LogisticRegression(penalty='l2', fit_intercept=False, C=1.0, max_iter=100, tol=1e-4)
     algo.fit(x_train, y_train)
 
