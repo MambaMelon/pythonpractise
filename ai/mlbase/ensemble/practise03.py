5a7bbb4a6e74b038a6919b37066cbe5b7d91e278
MambaMelon <1115431474@qq.com>
2018-07-05 14:37:07 +0800

    随机森林用于宫颈癌预测(多个目标属性)

--- ai/mlbase/ensemble/practise03.py
+++ ai/mlbase/ensemble/practise03.py
@@ -77,8 +77,6 @@ if __name__ == '__main__':
     # prodict_proba输出概率
     forest_y_score = forest.predict_proba(x_test)
 
-    print(forest_y_score)
-
     # 计算ROC值
     forest_fpr1, forest_tpr1, _ = metrics.roc_curve(
         label_binarize(y_test[names[-4]], classes=(0, 1, 2)).T[0:-1].T.ravel(), forest_y_score[0].ravel())
@@ -99,3 +97,59 @@ if __name__ == '__main__':
     print("Citology目标属性AUC值：", auc3)
     print("Biopsy目标属性AUC值：", auc4)
 
+    # 画图（ROC图）
+    plt.figure(figsize=(8, 6), facecolor='w')
+    plt.plot(forest_fpr1, forest_tpr1, c='r', lw=2, label=u'Hinselmann目标属性,AUC=%.3f' % auc1)
+    plt.plot(forest_fpr2, forest_tpr2, c='b', lw=2, label=u'Schiller目标属性,AUC=%.3f' % auc2)
+    plt.plot(forest_fpr3, forest_tpr3, c='g', lw=2, label=u'Citology目标属性,AUC=%.3f' % auc3)
+    plt.plot(forest_fpr4, forest_tpr4, c='y', lw=2, label=u'Biopsy目标属性,AUC=%.3f' % auc4)
+    plt.plot((0, 1), (0, 1), c='#a0a0a0', lw=2, ls='--')
+    plt.xlim(-0.001, 1.001)
+    plt.ylim(-0.001, 1.001)
+    plt.xticks(np.arange(0, 1.1, 0.1))
+    plt.yticks(np.arange(0, 1.1, 0.1))
+    plt.xlabel('False Positive Rate(FPR)', fontsize=16)
+    plt.ylabel('True Positive Rate(TPR)', fontsize=16)
+    plt.grid(b=True, ls=':')
+    plt.legend(loc='lower right', fancybox=True, framealpha=0.8, fontsize=12)
+    plt.title(u'随机森林多目标属性分类ROC曲线', fontsize=18)
+    # plt.show()
+
+    # 比较不同树数目、树最大深度的情况下随机森林的正确率
+    # 一般情况下，初始的随机森林树个数是100，深度1，如果需要我们再进行优化操作
+    x_train2, x_test2, y_train2, y_test2 = train_test_split(X, Y, test_size=0.5, random_state=0)
+    print("训练样本数量%d，测试样本数量:%d" % (x_train2.shape[0], x_test2.shape[0]))
+    estimators = [1, 50, 100, 500]
+    depth = [1, 2, 3, 7, 15]
+    err_list = []
+    for es in estimators:
+        es_list = []
+        for d in depth:
+            tf = RandomForestClassifier(n_estimators=es, criterion='gini', max_depth=d, max_features=None,
+                                        random_state=0)
+            tf.fit(x_train2, y_train2)
+            st = tf.score(x_test2, y_test2)
+            err = 1 - st
+            es_list.append(err)
+            print("%d决策树数目，%d最大深度，正确率:%.2f%%" % (es, d, st * 100))
+        err_list.append(es_list)
+
+    plt.figure(facecolor='w')
+    i = 0
+    colors = ['r', 'b', 'g', 'y']
+    lw = [1, 2, 4, 3]
+    max_err = 0
+    min_err = 100
+    for es, l in zip(estimators, err_list):
+        plt.plot(depth, l, c=colors[i], lw=lw[i], label=u'树数目:%d' % es)
+        max_err = max((max(l), max_err))
+        min_err = min((min(l), min_err))
+        i += 1
+    plt.xlabel(u'树深度', fontsize=16)
+    plt.ylabel(u'错误率', fontsize=16)
+    plt.legend(loc='upper left', fancybox=True, framealpha=0.8, fontsize=12)
+    plt.grid(True)
+    plt.xlim(min(depth), max(depth))
+    plt.ylim(min_err * 0.99, max_err * 1.01)
+    plt.title(u'随机森林中树数目、深度和错误率的关系图', fontsize=18)
+    plt.show()
