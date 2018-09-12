c92426854209940ed1deb4ff1660cd03b680a69b
MambaMelon <1115431474@qq.com>
2018-09-12 21:33:16 +0800

    操作excel

--- ai/pythonbase/readexcel.py
+++ ai/pythonbase/readexcel.py
@@ -9,7 +9,9 @@ if __name__ == '__main__':
     files = './datasets/excel.xls'
     wb = xlrd.open_workbook(files)
     ws = wb.sheet_by_name('Sheet1')
-
+    cell = ws.cell(1, 2)
+    print(cell.ctype)
+    print(cell.value)
     dataset = []
     for r in range(ws.nrows):
         col = []
