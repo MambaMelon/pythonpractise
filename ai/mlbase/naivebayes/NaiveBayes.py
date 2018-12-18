62005608dfa06fe316eee59c3c1047c2e57e3004
MambaMelon <1115431474@qq.com>
2018-12-18 18:02:22 +0800

    朴素贝叶斯算法实现

---
+++ ai/mlbase/naivebayes/NaiveBayes.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/18 16:16
+# @Author  : melon
+
+import math
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+
+def create_data():
+    iris = load_iris()
+    df = pd.DataFrame(iris.data, columns=iris.feature_names)
+    df['label'] = iris.target
+    df.columns = ['sepal length', 'sepal width', 'petal length', 'petal width', 'label']
+    data = np.array(df.iloc[:100, :])
+    # print(data)
+    return data[:,:-1], data[:,-1]
+
+class NaiveBayes:
+
+    def __init__(self):
+        self.model = None
+
+    # 数学期望(平均值)
+    @staticmethod
+    def mean(X):
+        return sum(X) / float(len(X))
+
+    # 标准差
+    def stdev(self, X):
+        avg = self.mean(X)
+        return math.sqrt(sum([pow(x-avg, 2) for x in X]) / float(len(X)))
+
+    # 概率密度函数
+    def gaussian_probability(self, x, mean, stdev):
+        exponent = math.exp(-(math.pow(x-mean, 2)/(2*math.pow(stdev, 2))))
+        return (1 / (math.sqrt(2*math.pi)*stdev)) * exponent
+
+    # 处理X_train
+    def summarize(self, train_data):
+        summaries = [(self.mean(i), self.stdev(i)) for i in zip(*train_data)]
+
+    # 分别求出数学期望和标准差
+    def fit(self, X, y):
+        labels = list(set(y))
+        data = {label: [] for label in labels}
+        for f, label in zip(X, y):
+            data[label].append(f)
+        self.model = {label: self.summarize(value) for label, value in data.items()}
+        return 'gaussianNB train done!'
+
+    # 计算概率
+    def calculate_probabilities(self, input_data):
+        probabilities = {}
+        for label, value in self.model.items():
+            probabilities[label] = 1
+            for i in range(len(value)):
+                mean, stdev = value[i]
+                probabilities[label] *= self.gaussian_probability(input_data[i], mean, stdev)
+        return probabilities
+
+    def predict(self, X_test):
+        label = sorted(self.calculate_probabilities(X_test).items(), key=lambda x: x[-1])[-1][0]
+        return label
+
+    def score(self, X_test, y_test):
+        right = 0
+        for X, y in zip(X_test, y_test):
+            label = self.predict(X)
+            if label == y:
+                right += 1
+        return right / float(len(X_test))
+
+if __name__ == '__main__':
+
+    X, y = create_data()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+    model = NaiveBayes()
+    model.fit(X_train, y_train)
+
+    print(model.predict([4.4,  3.2,  1.3,  0.2]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
